85b202063a0ba053fc8070ac6e90bcdc40f67716
Skalnark <skalnark@live.com>
2018-04-17 12:08:25 -0300

    Camera Control first script added, Utils modified, some materials and textures added

---
+++ Assets/myScripts/CameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    public GameObject machine;
+    //public GameObject spotlight;
+    private Vector3 right, left;
+    public TextMesh cellNumber;
+	void Start () {
+        machine.GetComponent<Transform>();
+        //spotlight.GetComponent<Transform>();
+        right = new Vector3(1.5f, 0, 0);
+        left = new Vector3(-1.5f, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            machine.transform.position += right;
+            //Utils.Wait(3);
+            //Debug.Log("Rodou?");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            machine.transform.position += left;
+        }
+    }
+ 
+}
---
+++ Assets/myScripts/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour {
+
+    public Alphabet alph;
+    public TuringMachine tm;
+    public int tapecellnumb;
+
+    public static IEnumerator Wait(int time)
+    {
+        yield return new WaitForSecondsRealtime(time);
+    }
+
+    public static IEnumerator Blink(GameObject light, int time)
+    {
+        light.SetActive(false);
+        yield return new WaitForSecondsRealtime(time);
+        light.SetActive(true);
+    }
+}
